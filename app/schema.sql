26d2c2a74544a574bf9fa7570b52ac24cc5188de
eugenio <eugenio.bancaro@gmail.com>
2016-01-15 16:37:25 +0100

    cleanup

--- app/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists registry;
-create table registry (
-  id integer primary key autoincrement,
-  token text not null,
-  endpoint text not null,
-  channel text,
-  phoneNumber integer not null,
-  pid integer
-);
